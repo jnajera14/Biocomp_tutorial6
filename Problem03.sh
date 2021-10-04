6aa63a05a4603e5f8b9a49c7970401ed4a07cc3b
Julian Najera <juliannajera14@gmail.com>
2021-10-04 14:40:03 -0400

    Integrated changes from Github on local repository

--- Problem03.sh
+++ Problem03.sh
@@ -4,7 +4,5 @@
 val1=$(cat "$1" | grep -v "gender" | cut -d , -f 3,4 | sort -n | sed 's/,/ /g' | grep "12 " | head -n 1 | cut -d " " -f 2)
 val2=$(cat "$1" | grep -v "gender" | cut -d , -f 3,4 | sort -n | sed 's/,/ /g' | grep "16 " | head -n 1 | cut -d " " -f 2)
 
-diff="$val2-$val1"
-
 echo "The effect of graduating college (12 vs. 16 years) on minimum wage is a difference of:" 
 echo "$val2 $val1" | awk '{print $1-$2}' 
